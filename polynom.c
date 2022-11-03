fc3eeff6bda0e3dda0bf849ada150373d286ef84
Artem <artem.zx656@gmail.com>
2022-11-03 22:05:29 +0300

    added support for large numbers

--- polynom.c
+++ polynom.c
@@ -3,31 +3,30 @@
 int main(int argc, char **argv)
 {
     
-    int n;
-    double x0;
-    double pol;
-    double dpol;
-    printf("Enter n:\n");
-    scanf("%i", &n);
-    printf("Enter x0:\n");
-    scanf("%lF", &x0);
-    int arr[n];
-    for (int i = n; i >= 0; i--)
+    long long n;
+    long long x0;
+    long long pol;
+    long long dpol;
+    //printf("Enter n:\n");
+    scanf("%lld", &n);
+    //printf("Enter x0:\n");
+    scanf("%lld", &x0);
+    long long arr[n];
+    for (long long i = n; i >= 0; i--)
     {
-        printf("Enter a%i:\n", i);
-        scanf("%i", &arr[i]);
+        //printf("Enter a%i:\n", i);
+        scanf("%lld", &arr[i]);
     }
     pol = arr[n] * x0 + arr[n - 1];
-    for (int j = 2; j <= n; j++)
+    for (long long j = 2; j <= n; j++)
     {
         pol = pol * x0 + arr[n - j];
     }
-    printf("%lF\n", pol);
+    printf("%lld\n", pol);
     dpol = n * arr[n] * x0 + arr[n - 1] * (n - 1);
-    for (int j = n - 2; j >= 1; j--)
+    for (long long j = n - 2; j >= 1; j--)
     {
         dpol = dpol * x0 + arr[j] * j;
     }
-    printf("%lF", dpol);
-    return 0;
-}
+    printf("%lld", dpol);
+    return 0;}
