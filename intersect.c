59013f34ac09c18927928606102bcfc2a874550a
Artem <artem.zx656@gmail.com>
2022-11-06 03:09:15 +0300

    removed unused libraries

--- intersect.c
+++ intersect.c
@@ -1,9 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
-#include <wchar.h>
-#include <stddef.h>
 
 unsigned long scanSet(int size)
 {
@@ -27,7 +22,7 @@ void printSet(unsigned long set)
 {
     for (int i = 0; i <= 31; i++)
     {
-        if((set | (1 << i)) == set)
+        if ((set | (1 << i)) == set)
         {
             printf("%d ", i);
         }
