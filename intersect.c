250c812647cf1199e074a63058356eed02f4f742
Artem <artem.zx656@gmail.com>
2022-11-06 03:06:14 +0300

    added intersect.c

---
+++ intersect.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <wchar.h>
+#include <stddef.h>
+
+unsigned long scanSet(int size)
+{
+    unsigned long result = 0;
+    int element;
+    for (int i = 0; i < size; i++)
+    {
+        scanf("%d", &element);
+        result = (result | (1 << element));
+    }
+    return result;
+}
+
+unsigned long intersectSets(unsigned long a, unsigned long b)
+{
+    unsigned long result = a & b;
+    return result;
+}
+
+void printSet(unsigned long set)
+{
+    for (int i = 0; i <= 31; i++)
+    {
+        if((set | (1 << i)) == set)
+        {
+            printf("%d ", i);
+        }
+    }
+}
+
+int main(int argc, char **argv)
+{
+    int sizeA, sizeB;
+    unsigned long A, B, result;
+
+    scanf("%d", &sizeA);
+    A = scanSet(sizeA);
+
+    scanf("%d", &sizeB);
+    B = scanSet(sizeB);
+
+    printSet(intersectSets(A, B));
+
+    return 0;
+}
