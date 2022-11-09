844f5f4af89647a7719ff633bd7c28a6192c418d
Artem <artem.zx656@gmail.com>
2022-11-09 15:18:00 +0300

    fixed memory leak

--- primediv.c
+++ primediv.c
@@ -5,7 +5,7 @@
 int main(int argc, char **argv)
 {
     long long n;
-    //printf("input: ");
+    // printf("input: ");
     scanf("%lld", &n);
     n = llabs(n);
     char *a = malloc(n);
@@ -14,19 +14,19 @@ int main(int argc, char **argv)
     {
         a[i] = 1;
     }
-    
-    //printf ("test1\n");
-    for (long long i = 2; i*i <= n; i++)
+
+    // printf ("test1\n");
+    for (long long i = 2; i * i <= n; i++)
     {
         if (a[i] == 1)
         {
-            for (long long j = i * i; j <= n; j += i )
+            for (long long j = i * i; j <= n; j += i)
             {
                 a[j] = 0;
             }
         }
     }
-    //printf ("test2\n");
+    // printf ("test2\n");
 
     for (long long i = n; i >= 2; i--)
     {
@@ -34,23 +34,23 @@ int main(int argc, char **argv)
         {
             if (n % i == 0)
             {
-                printf ("%lld ", i);
+                printf("%lld ", i);
+                free(a);
                 return 0;
             }
         }
-        
     }
-    
-    
+
     /*for (long long i = 2; i <= n; i++)
     {
         if (a[i] == 1)
         {
             printf("%lld ", i);
         }
-        
-    }*/ // рабочий вариант вывода простых чисел
-    //printf ("\ntest3\n");
+
+    }*/
+    // рабочий вариант вывода простых чисел
+    // printf ("\ntest3\n");
     /*for (long long j = 0; j <= n; j++) // вывод массива для отладки
     {
         printf("%lld ", a[j]);
