7d138299e46338fe2fba7d93d32fc3248c47694d
Artem Zhuk <artem.zx656@gmail.com>
2022-11-23 03:40:15 +0300

    added check for all mallocs

--- prefixes.c
+++ prefixes.c
@@ -7,7 +7,7 @@
 
 
 void prefixFunction(const char* string, size_t length, int* p, int* result)// string - initial string,
-// length - its length, p - prefix function result, result -
+// length - its length, p - prefix function result, result[i] = j : i =
 {
     int k , i;
     for(i = 1; i <(int)length; i++)//creating prefix array
@@ -35,22 +35,18 @@ int main(int argc, char **argv)
 {
     size_t length = strlen(argv[1]);
     char *string = malloc(length + 1);
-    if(string == NULL)
-    {
-        printf("failed to malloc\n");
-        return 0;
-    }
     strcpy(string, argv[1]);
 
     int *p = calloc(length, sizeof(int));// array for prefix function of string
     int *result = calloc(length + 1, sizeof(int));
 
-    prefixFunction(string, length, p, result);
+    if(string == NULL | p == NULL | result == NULL)
+    {
+        printf("failed to malloc\n");
+        return 0;
+    }
 
-//    for(int i = 0; i < length; i++) // printing prefix array
-//    {
-//        printf("%d ", p[i]);
-//    }
+    prefixFunction(string, length, p, result);
 
     for(int i = 0; i <= length; i++)
     {
@@ -63,29 +59,3 @@ int main(int argc, char **argv)
     free(result);
     return  0;
 }
-
-//    int k;
-//    p[0] = 0;
-//    char *substring1 = malloc(length);
-//    char *substring2 = malloc(length);
-//    int i = 1;
-//    while(i < (int)length)
-//    {
-//        k = i;
-//        substring(string, length, substring1, 0, k - 1);
-//        substring(string, length, substring2, i - k + 1, i);
-//        while( k > 0 && strcmp(substring1, substring2) != 0)
-//        {
-//            substring(string, length, substring1, 0, k -1);
-//            substring(string, length, substring2, i - k + 1, i);
-//            k -= 1;
-//        }
-//        p[i] = k;
-//        i += 1;
-//    }
-
-//void substring(char* string, size_t length, char* substring, int i, int j)
-//{
-//    memset(substring, 0, length);
-//    memcpy(substring, string + i, j - i);
-//}
