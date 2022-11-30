b8330baa99198a8400b96b0d3838efc6b0e72e36
Artem Zhuk <artem.zx656@gmail.com>
2022-12-01 02:00:51 +0300

    working and tested datesort!

--- datesort.c
+++ datesort.c
@@ -1,72 +1,113 @@
-#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
-#include <wchar.h>
 
 struct Date {
     int Day, Month, Year;
 };
 
+void scanStruct(struct Date *dest, int nel);
+void printStruct(struct Date *dest, int nel);
+void dsort(const int *key, int radix, struct Date *source, int n, struct Date *dest);
+void generateKeys(struct Date *source, int *keys, int n, int id, int *radix);
+void radixSort(struct Date *source, struct Date *dest, int q, int n);
+
+int main() {
+    int nel;
+    scanf("%d", &nel);
+    struct Date dateSource[nel];
+    struct Date dateDest[nel];
+    scanStruct(dateSource, nel);
+    radixSort(dateSource, dateDest, 6, nel);// q = 6 for dates(2 for day, month, year)
+    printStruct(dateDest, nel);
+    return 0;
+}
+
 void scanStruct(struct Date *dest, int nel) {
     for (int i = 0; i < nel; i++) {
         scanf("%04d %02d %02d", &dest[i].Year, &dest[i].Month, &dest[i].Day);
+    }
+    for (int i = 0; i < nel; i++) {
+        dest[i].Year -= 1970;
     }
 }
 
 void printStruct(struct Date *dest, int nel) {
     for (int i = 0; i < nel; i++) {
-        printf("%04d %02d %02d \n", dest[i].Year, dest[i].Month, dest[i].Day);
+        dest[i].Year += 1970;
+        printf("%04d %02d %02d", dest[i].Year, dest[i].Month, dest[i].Day);
+        printf("\n");
     }
 }
 
-void dsort(struct Date *keys, struct Date *dest, int n, int m) { //n-nel, m - number of keys
+void dsort(const int *key, int radix, struct Date *source, int n, struct Date *dest) { //n-nel, m - number of keys
     int k;
     int j = 0;
     int i = 1;
-    int *count = calloc(m, sizeof(int));
-    if (count == NULL)
-        printf("failed to malloc");
+    int *count = calloc(radix, sizeof(int));
 
     while (j < n) {
-        k = (int) source[j].Day;
+        k = key[j];
         count[k] = count[k] + 1;
         j += 1;
     }
-    while (i < m) {
+    while (i < radix) {
         count[i] = count[i] + count[i - 1];
         i += 1;
     }
     j = n - 1;
 
     for (int g; j >= 0; j--) {
-        k = (int) source[j].Day;
+        k = (int) key[j];
         g = count[k] - 1;
         count[k] = g;
-        dest[g].Day = source[j].Day;
+        dest[g] = source[j];
     }
     free(count);
 }
 
-void radixSort(struct Date *source, struct Date *dest, int q, int n, int m){
-    memcpy(dest, source, n * 12);
-    for(int i = q - 1; i >= 0; i--){
-        dsort(dest, dest, n, m);
+void generateKeys(struct Date *source, int *keys, int n, int id, int *radix) {
+    for (int i = 0; i < n; i++) {
+        switch (id) {
+            case 0:
+                keys[i] = source[i].Year / 10; // first digit of year -1970
+                *radix = 7;
+                break;
+            case 1:
+                keys[i] = source[i].Year % 10; // second digit of year -1970
+                *radix = 10;
+                break;
+            case 2:
+                keys[i] = source[i].Month / 10; // first digit of month
+                *radix = 2;
+                break;
+            case 3:
+                keys[i] = source[i].Month % 10; // second digit of month
+                *radix = 10;
+                break;
+            case 4:
+                keys[i] = source[i].Day / 10; // first digit of day
+                *radix = 4;
+                break;
+            case 5:
+                keys[i] = source[i].Day % 10; // second digit of day
+                *radix = 10;
+                break;
+            default:
+                printf("keys function error");
+        }
     }
-
 }
 
-int main() {
-    int nel;
-    scanf("%d", &nel);
+void radixSort(struct Date *source, struct Date *dest, int q, int n) {
+    int keys[n];
+    int *radix = malloc(sizeof(int)); //number of digits in single position (razryad?)
+    memcpy(dest, source, sizeof(struct Date) * n);
 
-    struct Date dateSource[nel];
-    struct Date dateDest[nel];
-    scanStruct(dateSource, nel);
-    dsort(dateSource, dateDest, nel, 31);
-    printStruct(dateDest, nel);
-
-
-    return 0;
+    for (int id = q - 1; id >= 0; id--) {
+        generateKeys(source, keys, n, id, radix);
+        dsort(keys, *radix, source, n, dest);
+        memcpy(source, dest, sizeof(struct Date) * n);
+    }
+    free(radix);
 }
