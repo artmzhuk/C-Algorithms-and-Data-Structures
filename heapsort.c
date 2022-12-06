c4cf925be56f39354308ae0c9a374e01d8130b10
Artem Zhuk <artem.zx656@gmail.com>
2022-12-07 01:47:55 +0300

    added lection hsort

--- heapsort.c
+++ heapsort.c
@@ -3,12 +3,67 @@
 #include <stdlib.h>
 #include <string.h>
 
-void heapify(void *base, int i, size_t nel, size_t width);
-void hsort(void *base, size_t nel, size_t width,
-           int (*compare)(const void *a, const void *b));
-void swap(void *base, int a, int b, size_t width);
-int compare(const void *a, const void *b);
+int compare(const void *a, const void *b)
+{
+    int counterA = 0;
+    int counterB = 0;
+    char elementA = *(char*)a;
+    char elementB = *(char*)b;
+    for(int i = 0; i < 1001 && elementA != '\0'; i++)
+    {
+        if(elementA == 'a')
+            counterA += 1;
+        elementA = *(char*)(a + i);
+    }
+    for(int i = 0; i < 1001 && elementB != '\0'; i++)
+    {
+        if(elementB == 'a')
+            counterB += 1;
+        elementB = *(char*)(b + i);
+    }
+    return counterA - counterB;
+}
 
+void swap(void *base, int a, int b, size_t width)
+{
+    void *swapBuffer = malloc(width);
+    memcpy(swapBuffer, base + a * width, width);
+    memcpy(base + a * width, base + b * width, width);
+    memcpy(base + b * width, swapBuffer, width);
+    free(swapBuffer);
+}
+
+void heapify(void* base, size_t nel, size_t width, int i){
+    while (1 == 1){
+        int l = 2 * i + 1;
+        int r = l + 1;
+        int j = i;
+        if (l < nel && (compare(base + i * width, base + l * width) < 0))
+            i = l;
+        if (r < nel && (compare(base + i * width, base + r * width) < 1))
+            i = r;
+        if (i == j)
+            break;
+        swap(base, i, j, width);
+    }
+}
+
+void buildheap (void* base, size_t nel, size_t width){
+    for(int i = ((int)nel / 2) - 1; i >= 0; i--)
+        heapify(base, nel, width, i);
+}
+
+
+
+void hsort(void *base, size_t nel, size_t width,
+           int (*compare)(const void *a, const void *b))
+{
+    buildheap(base, nel, width);
+    for(int i = (int)nel - 1; i > 0; i--){
+        swap(base, 0, i, width);
+        heapify(base, i, width, 0);
+    }
+}
 
 int main()
 {
@@ -30,6 +85,7 @@ int main()
     return 0;
 }
 
+/*
 void heapify(void *base, int i, size_t nel, size_t width)
 {
     int left = 2 * i + 1;
@@ -98,3 +154,4 @@ int compare(const void *a, const void *b)
     }
     return counterA - counterB;
 }
+*/
